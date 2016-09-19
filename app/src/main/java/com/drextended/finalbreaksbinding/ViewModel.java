266d7d10b36cd310aad1fa5067ae6331da1a34ec
roman.donchenko <donchenko@river-soft.net>
2016-09-19 11:16:20 +0300

    comments + apk

--- app/src/main/java/com/drextended/finalbreaksbinding/ViewModel.java
+++ app/src/main/java/com/drextended/finalbreaksbinding/ViewModel.java
@@ -10,9 +10,9 @@ public class ViewModel {
     // As it's saing in doc: Fields of this type should be declared final
     // because bindings only detect changes in the field's value, not of the field itself
     // https://developer.android.com/reference/android/databinding/ObservableBoolean.html
-    public static final ObservableField<String> finalVar = new ObservableField<>();
+    public static final ObservableField<String> finalVar = new ObservableField<>(); // break binding
 
-    public static ObservableField<String> nonFinalVar = new ObservableField<String>();
+    public static ObservableField<String> nonFinalVar = new ObservableField<>(); // works
 
     public void setFinalVar(boolean checked) {
         finalVar.set(String.valueOf(checked));
